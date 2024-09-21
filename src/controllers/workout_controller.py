bbd87f5a0bb2bc69d92a684b54bd4f9d82ba71fb
Jessicavazm <jjessicavaz@gmail.com>
2024-09-22 09:58:06 +1000

    Created routes for GET workouts/ workout

--- src/controllers/workout_controller.py
+++ src/controllers/workout_controller.py
@@ -3,7 +3,7 @@ from datetime import date
 from flask import Blueprint, request
 from flask_jwt_extended import jwt_required, get_jwt_identity
 
-from models.workout import Workout, workout_schema, WorkoutSchema
+from models.workout import Workout, workout_schema, workouts_schema, WorkoutSchema
 from init import db, bcrypt
 
 from sqlalchemy.exc import IntegrityError
@@ -13,12 +13,36 @@ from psycopg2 import errorcodes
 workout_bp = Blueprint("workout", __name__,url_prefix="/workout")
 
 
-# Create 'register'  workout route, JWT required
+# Create route for 'GET' workouts
+@workout_bp.route("/")
+def get_all_workouts():
+    # Create and execute stmt, order by descending order
+    stmt = db.select(Workout).order_by(Workout.date.desc())
+    workouts = db.session.scalars(stmt)
+    # Serialise data using workouts_schema
+    return workouts_schema.dump(workouts), 200
+
+
+# Create route for 'GET' a specific workout
+@workout_bp.route("/<int:workout_id>")
+def get_a_workout(workout_id):
+    # Use filter_by to select a specific card
+    # stmt = db.select(Card).where(Card.id==card_id)
+    stmt = db.select(Workout).filter_by(id=workout_id)
+    workout = db.session.scalar(stmt)
+    # If workout returns workout, Else returns error message
+    if workout:
+        return workout_schema.dump(workout)
+    else:
+        return {"error": f"Card with {workout_id} not found."}, 404
+
+
+# Create 'register' workout route, JWT required, 'POST' method to insert data into DB
 @workout_bp.route("/register", methods=["POST"])
 @jwt_required()
 def register_workout():
     try:
-        # Get the fields from body of request
+        # Get the fields from body of request, deserialise using workout_schema
         body_data = workout_schema.load(request.get_json())
         # Create a new workout instance
         workout = Workout(
@@ -33,9 +57,12 @@ def register_workout():
         db.session.commit()
         # Return acknowledgment message
         return workout_schema.dump(workout), 201
+    # Display personalised messages in case of data violations
     except IntegrityError as err:
-        # Display personalised messages in case of data violations
         if err.orig.pgcode == errorcodes.NOT_NULL_VIOLATION:
             return{"error": f"The column {err.orig.diag.column_name} is required"}, 400
     
 
+# Create route for updating workout
+
+# Create route for deleting workout
