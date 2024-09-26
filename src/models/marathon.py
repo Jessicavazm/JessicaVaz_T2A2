c983979d6032c204728c9ab66b0393977080405a
Jessicavazm <jjessicavaz@gmail.com>
2024-09-26 11:40:00 +1000

    Added validation on tables attributes

--- src/models/marathon.py
+++ src/models/marathon.py
@@ -1,5 +1,5 @@
 from marshmallow import fields
-from marshmallow.validate import Length, And, Regexp, Range
+from marshmallow.validate import Length, And, Regexp
 
 from init import db, ma
 
@@ -12,7 +12,7 @@ class Marathon(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20), nullable=False)
     event_date = db.Column(db.Date, nullable=False)
-    location = db.Column(db.String, nullable=False)
+    location = db.Column(db.String(50), nullable=False)
     distance_kms = db.Column(db.Integer, nullable=False)
     
     # Define bidirectional relationships with 'logs' table
@@ -25,11 +25,14 @@ class Marathon(db.Model):
 class MarathonSchema(ma.Schema):
     marathon_logs = fields.List(fields.Nested("MarathonLogSchema", only=["id", "entry_created"]))
     
-    # Validation for attribute 'name', 
-    # Name containing two names is allowed eg: 'Coder academy'
+    # Validation for 'name'. Name containing two names is allowed eg: 'Coder academy'
     name = fields.String(required=True, validate=And(Length(min=4, max=20, error="Name must be between 4 and 20 characters in length."), Regexp("^[A-Z][a-zA-Z]*( [A-Z][a-zA-Z]*)*$", error="Name must start with an uppercase letter and contain only letters.")))
     
-    
+    # Validation for attribute 'location'
+    # Allows letter, numbers, spaces, apostrophes and commas eg: Location: 12 O'Connor st, Melbourne
+    location = fields.String(required=True, validate=And(Length(min=10, max=50, error="Location must be between 10 and 50 characters in length."), Regexp(r"^(?=(.*[a-zA-Z]){2,})(?!.*([', ])\2)[a-zA-Z0-9]+(?:[', ]+[a-zA-Z0-9]+)*$", error="Location must contain letters and may include numbers, spaces, apostrophes, and commas. Consecutive apostrophes, commas and spaces are not allowed.")))
+
+
     class Meta:
         fields = ["id", "name", "event_date", "location", "distance_kms", "marathon_logs"]
         ordered = True
--- src/models/user.py
+++ src/models/user.py
@@ -1,7 +1,7 @@
 from init import db, ma
 
 from marshmallow import fields
-from marshmallow.validate import Regexp
+from marshmallow.validate import Length, And, Regexp
 
 
 class User(db.Model):
@@ -10,7 +10,7 @@ class User(db.Model):
 
     # Attributes
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(30), nullable=False)
+    name = db.Column(db.String(20), nullable=False)
     email = db.Column(db.String(50), nullable=False, unique=True)
     password = db.Column(db.String, nullable=False)
     is_admin = db.Column(db.Boolean, default=False)
@@ -21,12 +21,25 @@ class User(db.Model):
     workouts = db.relationship("Workout", back_populates = "user", cascade="all, delete")
     group_logs = db.relationship("GroupLog", back_populates="user", cascade="all, delete")
 
+
 # Define 'user' schema and class 'Meta' fields to serialize/ deserialize data
 # Unpack complex data with fields.Nested method, fields.List to unpack a list of objects
 # Exclude 'user' from workouts and group_logs schemas to avoid redundant data
 class UserSchema(ma.Schema):
     workouts = fields.List(fields.Nested("WorkoutSchema", exclude=["user"]))
     group_logs = fields.List(fields.Nested("GroupLogSchema", exclude=["user"]))
+
+
+    # User name validation, name containing two names is allowed eg: 'Coder academy'
+    name = fields.String(required=True, validate=And(Length(min=2, max=20, error="Name must be between 2 and 20 characters in length."), Regexp("^[A-Z][a-zA-Z]*( [A-Z][a-zA-Z]*)*$", error="Name must start with an uppercase letter and contain only letters.")))
+
+    # User email validation
+    email = fields.String(required=True, validate=And(Length(min=5, max=50, error="Email must be between 5 and 50 characters in length."), Regexp("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$", error="Invalid email format: The email cannot have consecutive dots, must have a local part, a non-empty domain name and a top-level domain containing at least two letters. Valid characters includes letters, numbers, underscore, period, percent sign, plus sign and hyphen.")))
+
+    # User password validation
+    password = fields.String(required=True, validate=And(Length(min=6, max=20, error="Password must be a minimum of 6 characters and maximum of 20 characters."), Regexp(r"^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=\[\]{};':\"\\|,.<>\/?])[A-Za-z\d!@#$%^&*()_+\-=\[\]{};':\"\\|,.<>\/?]{6,}$", error="Invalid password format. Password must contain one upper case letter, one digit and one special character.")))
+
+
     class Meta:
         fields = ["id", "name", "email", "password", "is_admin", "workouts", "group_logs"]
         ordered = True 
